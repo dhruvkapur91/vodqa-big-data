738b00da24788588c9eb9097685db73296ea9901
Dhruv Kapur <dhruvk@thoughtworks.com>
2016-11-30 13:33:32 +0530

    Remove test

--- src/test/scala/spark/WordCountRDDSpec.scala
+++ src/test/scala/spark/WordCountRDDSpec.scala
@@ -21,22 +21,6 @@ class WordCountRDDSpec extends TestWrapper {
     wordCount.count() should be(0)
   }
 
-  test("Should return a map of words along with their count") {
-    Given("A sequence of sentences")
-    val sentenceOne = "one sentence"
-    val sentenceTwo = "another sentence"
-    val sentences = Seq(sentenceOne,sentenceTwo)
-    val sentencesRDD: RDD[String] = sc.parallelize(sentences)
-
-    When("We do word count")
-    val wordCount: RDD[(String, Int)] = WordCountRDD.getWordCount(sentencesRDD)
-
-    Then("We expect right count for each word in Map")
-    wordCount.count() should be(3)
-    wordCount.lookup("one").head should be(1)
-    wordCount.lookup("another").head should be(1)
-    wordCount.lookup("sentence").head should be(2)
-  }
 }
 
 
