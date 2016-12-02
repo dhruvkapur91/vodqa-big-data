d9d0c222ad769e20bd6a76c72c1071568f3cc468
vivekmahajan <vivekmah@thoughtworks.com>
2016-12-02 14:08:10 +0530

    Modified WordCount problem to contain Scala basic syntax and added tests.

--- src/main/scala/justscala/WordCount.scala
+++ src/main/scala/justscala/WordCount.scala
@@ -1,9 +1,41 @@
 package justscala
 
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Map
+
+
 object WordCount {
-  def apply(sentences:Seq[String]) : Map[String,Int] = {
-    val words = sentences.flatMap(sentence => sentence.split(" "))
-    val groupedWords = words.groupBy(identity)
-    groupedWords.mapValues(_.size)
+
+  def wordCount(sentences: ListBuffer[String]) : Map[String, Int] = {
+    val words = getListOfWords(sentences)
+    val wordCount = getCountOfEachWord(words)
+    wordCount.foreach(println)
+    return wordCount
   }
+
+  def getListOfWords(sentences: ListBuffer[String]) : ListBuffer[String] = {
+    var words: mutable.ListBuffer[String] = ListBuffer[String]()
+    for (sentence <- sentences) {
+      val wordsOfCurrentSentence: Array[String] = sentence.split(" ")
+      for (word <- wordsOfCurrentSentence) {
+        words += word
+      }
+    }
+    return words
+  }
+
+  def getCountOfEachWord(words : ListBuffer[String]) : Map[String, Int] = {
+    var wordCount: Map[String, Int] = Map[String, Int]()
+
+    for (word <- words) {
+      if (wordCount.isDefinedAt(word)) {
+        wordCount(word) = wordCount(word) + 1
+      } else {
+        wordCount(word) = 1
+      }
+    }
+    return wordCount
+  }
+
 }
--- src/test/scala/justscala/WordCountSpec.scala
+++ src/test/scala/justscala/WordCountSpec.scala
@@ -1,10 +1,30 @@
 package justscala
 
-import org.scalatest.FunSuite
+import scala.collection.mutable.ListBuffer
+import scala.collection.mutable.Map
 
-class WordCountSpec extends FunSuite {
-  test("Should return count of 1 when given one word") {
-    val wordCount: Map[String, Int] = WordCount.apply(Seq("word"))
-    assert(wordCount.size == 1)
+import org.scalatest.{FunSuite, GivenWhenThen, ShouldMatchers}
+
+class WordCountSpec extends FunSuite with ShouldMatchers with GivenWhenThen {
+
+  test("Should return an empty Map when given empty List") {
+    WordCount.wordCount(ListBuffer.empty[String]) should be(empty)
+  }
+
+  test("Should give word count when given a non List") {
+
+    Given("A list of sentences")
+    val sentence1 = "one sentence"
+    val sentence2 = "second sentence"
+    val sentences = ListBuffer(sentence1,sentence2)
+
+    When("Word Count is called")
+    val wordCount : Map[String,Int] = WordCount.wordCount(sentences)
+
+    Then("Word Count should give right count for each word")
+    wordCount.size should be(3)
+    wordCount("one") should be(1)
+    wordCount("sentence") should be(2)
+    wordCount("second") should be(1)
   }
 }
